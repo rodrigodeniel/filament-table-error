dcf96d7cdc2222f5343d0aa9fb11afaef74b18a5
rodrigodeniel <102530635+rodrigodeniel@users.noreply.github.com>
2024-06-14 13:52:51 -0300

    Create CreateConveniados.php

---
+++ ConveniadosResource/Pages/CreateConveniados.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\Conveniados\ConveniadosResource\Pages;
+
+use App\Filament\Resources\Conveniados\ConveniadosResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateConveniados extends CreateRecord
+{
+    protected static string $resource = ConveniadosResource::class;
+	
+}
